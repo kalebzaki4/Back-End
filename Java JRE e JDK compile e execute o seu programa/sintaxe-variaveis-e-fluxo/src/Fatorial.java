8b7f477f8ad0ff7eeb288010dfbdc5a1afe3641e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-03-20 19:10:04 -0300

    Delete Fatorial.java

--- Java JRE e JDK compile e execute o seu programa/sintaxe-variaveis-e-fluxo/src/Fatorial.java
+++
@@ -1,9 +0,0 @@
-public class Fatorial {
-	public static void main(String[] args) {
-		int fatorial = 1;
-		for (int i = 1; i < 11; i++) {
-			fatorial *= i;
-			System.out.println("Fatorial de " + i + " = " + fatorial);
-		}
-	}
-}
