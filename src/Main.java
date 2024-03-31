6fa1e09a3d6c11a732af751aed7e28aee6e6a2b2
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-03-31 18:40:09 -0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Alô Mundo!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluindoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme de aventura com galã dos anos 80";
+    }
+}
