c45f6b5e5747628a0f5a03109f1db08ccae4892c
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-04-19 16:29:21 -0300

    Update Filme.java

--- Projeto-2/ScreenMatch/src/Filme.java
+++ Projeto-2/ScreenMatch/src/Filme.java
@@ -2,4 +2,16 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+    }
 }
