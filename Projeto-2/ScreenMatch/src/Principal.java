2ad005db4cbcb39bae2235ca43207d382e64448a
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-04-20 12:36:33 -0300

    Update Principal.java

--- Projeto-2/ScreenMatch/src/Principal.java
+++ Projeto-2/ScreenMatch/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -6,8 +7,7 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
-
-
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -18,5 +18,14 @@ public class Principal {
         //meuFilme.somaDasAvaliacoes = 10;
         //meuFilme.totalDeAvaliacoes = 1;
         //System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+
     }
 }
