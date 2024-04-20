6d74c381f0aa3719879b6fde5254533f626348da
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-04-20 12:21:16 -0300

    Update Principal.java

--- Projeto-2/ScreenMatch/src/Principal.java
+++ Projeto-2/ScreenMatch/src/Principal.java
@@ -3,16 +3,16 @@ import br.com.alura.screenmatch.modelos.Filme;
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
 
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        meuFilme.avalia(7);
         System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
         //meuFilme.somaDasAvaliacoes = 10;
