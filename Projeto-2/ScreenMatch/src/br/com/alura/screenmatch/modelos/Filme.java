e0b70ef7243f449818677025bec6742592f67f17
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-04-20 12:10:10 -0300

    Update Filme.java

---
+++ Projeto-2/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
