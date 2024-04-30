de5a0628211436e70a5d0b581661b2c518d6cbfc
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-04-29 23:16:21 -0300

    Create Filme.java

---
+++ Projeto-2/ScreenMatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme extends Titulo {
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
