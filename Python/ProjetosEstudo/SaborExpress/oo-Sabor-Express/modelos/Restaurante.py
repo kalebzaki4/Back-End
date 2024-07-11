fbf18b9f11ec86c3d4a423cc2f503d3ef574e31b
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-11 10:04:56 -0300

    Update Restaurante.py

---
+++ Python/ProjetosEstudo/SaborExpress/oo-Sabor-Express/modelos/Restaurante.py
@@ -0,0 +1,14 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurante_praca.ativo)
