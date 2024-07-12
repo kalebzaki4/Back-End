9e9e64c9921e35b946346d3feacb85752a65affb
kalebzaki4 <kalebzsantosk@gmail.com>
2024-07-12 10:52:37 -0300

    Update Restaurante.py

--- Python/ProjetosEstudo/SaborExpress/oo-Sabor-Express/modelos/Restaurante.py
+++ Python/ProjetosEstudo/SaborExpress/oo-Sabor-Express/modelos/Restaurante.py
@@ -1,14 +1,20 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print(restaurante_praca.ativo)
+Restaurante.listar_restaurantes()
