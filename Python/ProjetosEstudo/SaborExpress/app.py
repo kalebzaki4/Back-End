686222446a166e263d15b7c25deaad75b3d722fe
kalebzaki4 <kalebzsantosk@gmail.com>
2024-08-06 11:43:51 -0300

    Create app.py

---
+++ Python/ProjetosEstudo/SaborExpress/app.py
@@ -0,0 +1,17 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho',2.00,'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
+def main():
+    restaurante_praca.exibir_cardapio
+
+if __name__ == '__main__':
+    main()
