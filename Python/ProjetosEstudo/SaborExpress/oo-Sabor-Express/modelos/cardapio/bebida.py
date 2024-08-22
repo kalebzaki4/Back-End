bf4fd9ccaa5bea7203bf931870f1b03387f4bf1b
kalebzaki4 <kalebzsantosk@gmail.com>
2024-08-22 19:06:07 -0300

    Create bebida.py

---
+++ Python/ProjetosEstudo/SaborExpress/oo-Sabor-Express/modelos/cardapio/bebida.py
@@ -0,0 +1,12 @@
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Bebida(ItemCardapio):
+    def __init__(self,nome,preco,tamanho):
+        super().__init__(nome,preco)
+        self.tamanho = tamanho
+    
+    def __str__(self):
+        return self._nome
+
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.08)
