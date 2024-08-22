7a98b8ee5cae40f58f3d274cee176761c01b718e
kalebzaki4 <kalebzsantosk@gmail.com>
2024-08-22 19:06:11 -0300

    Create prato.py

---
+++ Python/ProjetosEstudo/SaborExpress/oo-Sabor-Express/modelos/cardapio/prato.py
@@ -0,0 +1,12 @@
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
+    def __init__(self,nome,preco,descricao):
+        super().__init__(nome,preco)
+        self.descricao = descricao 
+
+    def __str__(self):
+        return self._nome
+
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
