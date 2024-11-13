01374a8230f85115a9a3378f1876ec135cbfa801
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-11-13 10:59:01 -0300

    Update Main.java

--- Java/Projeto1/src/Main.java
+++ Java/Projeto1/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+        System.out.println(ano);
+        boolean incluidoNoPlano = false;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3 ;
+        System.out.println(media);
     }
 }
