cc193689c0efc2b2f86d6b1a893e44a3c6c0f1f8
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-11-21 16:59:23 -0300

    Update Condicional.java

--- Java/Projeto1/src/Condicional.java
+++ Java/Projeto1/src/Condicional.java
@@ -1,20 +1,20 @@
 public class Condicional {
     public static void main(String[] args) {
         int anoDeLancamento = 2022;
-        boolean inclidoNoPlano = true;
+        boolean incluidoNoPlano = false;
         double notaDoFilme = 8.1;
-        String tipoPlano = "gratis 3 meses";
+        String tipoPlano = "plus";
 
-        if (anoDeLancamento >= 2022){
+        if (anoDeLancamento >= 2022) {
             System.out.println("Lançamento que os clientes estão curtindo!");
         } else {
             System.out.println("Filme retrô que vale a pena assistir!");
         }
 
-        if (tipoPlano.equals("plus")) {
-            System.out.println("Você pode assistir tudo!");
-        } else {
-            System.out.println("Você não tem o nosso plano basico!");
-        }
+       if (incluidoNoPlano == true || tipoPlano.equals("plus")) {
+           System.out.println("Filme liberado");
+       } else {
+           System.out.println("Deve pagar a locação");
+       }
     }
 }
