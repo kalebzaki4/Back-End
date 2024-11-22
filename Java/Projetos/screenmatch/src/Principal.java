fa91acd7b35b3374022fa2bad238f939ee81c21b
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-11-22 14:21:06 -0300

    Create Principal.java

---
+++ Java/Projetos/screenmatch/src/Principal.java
@@ -0,0 +1,18 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 2015;
+        meuFilme.duracaoEmMinuto = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalAvaliacoes);
+
+        System.out.println(meuFilme.retornaMediaDasAvaliacoes());
+
+    }
+}
