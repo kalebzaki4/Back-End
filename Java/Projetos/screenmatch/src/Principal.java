65389eaca70c720c2a36d4542313d919b4b88139
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-11-25 15:03:57 -0300

    Update Principal.java

--- Java/Projetos/screenmatch/src/Principal.java
+++ Java/Projetos/screenmatch/src/Principal.java
@@ -1,3 +1,5 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -9,8 +11,7 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println("Total de Avaliações: " + meuFilme.getTotalAvaliacoes());
 
         System.out.println(meuFilme.retornaMediaDasAvaliacoes());
 
