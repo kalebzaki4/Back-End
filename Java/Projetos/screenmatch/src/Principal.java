d25ff5c476b68671e913af5a5be1393f446b8316
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-11-27 15:48:57 -0300

    Update Principal.java

--- Java/Projetos/screenmatch/src/Principal.java
+++ Java/Projetos/screenmatch/src/Principal.java
@@ -3,9 +3,9 @@ import br.com.alura.screenmatch.modelos.Filme;
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 2015;
-        meuFilme.duracaoEmMinuto = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(2015);
+        meuFilme.setDuracaoEmMinuto(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
