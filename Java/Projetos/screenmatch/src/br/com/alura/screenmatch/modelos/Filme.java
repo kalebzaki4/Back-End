9583723caf2a3d25c0b392432d3e620167075320
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-12-02 07:19:22 -0300

    Update Filme.java

--- Java/Projetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ Java/Projetos/screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,60 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalAvaliacoes;
-    private int duracaoEmMinuto;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalAvaliacoes() {
-        return totalAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getDuracaoEmMinuto() {
-        return duracaoEmMinuto;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setDuracaoEmMinuto(int duracaoEmMinuto) {
-        this.duracaoEmMinuto = duracaoEmMinuto;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalAvaliacoes++;
-    }
-
-    public double retornaMediaDasAvaliacoes(){
-        return somaDasAvaliacoes / totalAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
