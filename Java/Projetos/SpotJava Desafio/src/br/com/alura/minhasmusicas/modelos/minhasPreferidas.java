8c80a93c4a8a0a89520e239383b851b4ea7adafd
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-12-10 13:28:59 -0300

    Update minhasPreferidas.java

--- Java/Projetos/SpotJava Desafio/src/br/com/alura/minhasmusicas/modelos/minhasPreferidas.java
+++ Java/Projetos/SpotJava Desafio/src/br/com/alura/minhasmusicas/modelos/minhasPreferidas.java
@@ -1,8 +1,14 @@
 package br.com.alura.minhasmusicas.modelos;
 
-public class minhasPreferidas {
+class MinhasPreferidas {
 
-    public void inclui(Audio audio){
-        if (audio. )
+    public void inclui(Audio audio) {
+        if(audio.getClassificacao() >= 9) {
+            System.out.println(audio.getTitulo() + " é considerado sucesso absoluto" +
+                    " e preferido por todos!");
+        } else {
+            System.out.println(audio.getTitulo() + " também é um dos que todo mundo está " +
+                    "curtindo");
+        }
     }
 }
