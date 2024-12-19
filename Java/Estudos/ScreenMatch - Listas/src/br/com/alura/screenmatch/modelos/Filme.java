f6b9c4aa69423de9f16ef26701a1fc2ff3d39ab3
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2024-12-19 12:11:05 -0300

    Update Filme.java

--- Java/Estudos/ScreenMatch - Listas/src/br/com/alura/screenmatch/modelos/Filme.java
+++ Java/Estudos/ScreenMatch - Listas/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -5,8 +5,8 @@ import br.com.alura.screenmatch.calculos.Classificavel;
 public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
-    public Filme(String nome) {
-        this.setNome(nome);
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
     }
 
     public String getDiretor() {
@@ -20,5 +20,10 @@ public class Filme extends Titulo implements Classificavel {
     @Override
     public int getClassificacao() {
         return (int) pegaMedia() / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
