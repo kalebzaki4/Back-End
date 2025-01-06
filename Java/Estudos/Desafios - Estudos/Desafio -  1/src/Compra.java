bbbbabc907ffbb534c10e9b208da9da060bc0c31
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-01-06 10:44:22 -0300

    Desafio de estudos completo.

---
+++ Java/Estudos/Desafios - Estudos/Desafio -  1/src/Compra.java
@@ -0,0 +1,17 @@
+class Compra {
+    private String descricao;
+    private double valor;
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
