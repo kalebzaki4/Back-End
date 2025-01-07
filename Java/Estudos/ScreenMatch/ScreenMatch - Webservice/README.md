2a855154e2e34d583f27b9e44e50992b9b784361
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-01-07 11:55:54 -0300

    Projeto inicial para consumir APIS

---
+++ Java/Estudos/ScreenMatch/ScreenMatch - Webservice/README.md
@@ -0,0 +1,15 @@
+![thumbnail-Formação Java (4)](https://user-images.githubusercontent.com/66698429/226751112-f79aaa28-16c9-4561-8a15-0ac62ec9cb44.png)
+
+
+# Java: consumindo uma API, gravando arquivos e lidando com erros
+
+Projeto desenvolvido no quarto curso da formação Java da Alura
+
+
+## 🔨 Objetivos do projeto
+
+- Aprender a consumir uma API HTTP em Java;
+- Entender como receber e converter dados no formato JSON; 
+- Utilizar a biblioteca externa GSON para fazer serialização e desserialização de dados;
+- Lidar com erros e tratar exceções;
+- Conhecer o pacote java.io para manipulação de arquivos.
---
+++ Java/Estudos/ScreenMatch/ScreenMatch - Webservice/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,53 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        Filme f1 = filmeDoPaulo;
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            if (item instanceof  Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
+
+        List<String> buscaPorArtista = new LinkedList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação");
+        System.out.println(buscaPorArtista);
+        System.out.println("Lista de titulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
+    }
+}
