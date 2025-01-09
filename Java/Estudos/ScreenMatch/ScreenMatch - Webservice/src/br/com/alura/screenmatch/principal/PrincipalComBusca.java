f5bc51a5536fc0899688e89f95d8981a4c6e5cd8
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-01-09 11:01:56 -0300

    Update PrincipalComBusca.java

--- Java/Estudos/ScreenMatch/ScreenMatch - Webservice/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ Java/Estudos/ScreenMatch/ScreenMatch - Webservice/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,11 +7,20 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class PrincipalCComBusca {
-    public static void main(String[] args) {
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=85c44cfa";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
