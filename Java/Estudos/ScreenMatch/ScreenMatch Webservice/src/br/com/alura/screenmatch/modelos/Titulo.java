75d7f7f858b43dc175f100ee2ca3cba15b35e65c
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-01-20 10:23:57 -0300

    Projeto ScreenMatch webservice completo!

--- Java/Estudos/ScreenMatch/ScreenMatch Webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ Java/Estudos/ScreenMatch/ScreenMatch Webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -86,9 +86,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento + "," +
-                " duração " + duracaoEmMinutos;
-
+        return "(nome = " + nome +
+                ", anoDeLancamento = " + anoDeLancamento + "," +
+                " duração = " + duracaoEmMinutos + ")";
     }
 }
--- Java/Estudos/ScreenMatch/ScreenMatch Webservice/src/br/com/alura/screenmatch/modelos/TituloOmdb.java
+++ Java/Estudos/ScreenMatch/ScreenMatch Webservice/src/br/com/alura/screenmatch/modelos/TituloOmdb.java
@@ -1,7 +1,4 @@
 package br.com.alura.screenmatch.modelos;
 
 public record TituloOmdb(String title, String year, String runtime) {
-    public String getResponse() {
-        return "";
-    }
 }
