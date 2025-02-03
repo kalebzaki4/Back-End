503d8aa74a0c5b14d9236b0c6471778fb207f142
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-02-03 13:58:33 -0300

    Update ScreenmatchApplication.java

--- Java/Estudos/ScreenMatch/Formação - 2/ScreenMatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Java/Estudos/ScreenMatch/Formação - 2/ScreenMatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -8,13 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
-		principal.exibeMenu();
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.exibeMenu();
+    }
 }
