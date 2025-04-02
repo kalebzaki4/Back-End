d3445f31d53ac867bbc6de36ee63ab458724ec62
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-04-02 19:47:34 -0300

    Update userResolvers.js

--- GraphQL/graphql-config-inicial/api/user/resolvers/userResolvers.js
+++ GraphQL/graphql-config-inicial/api/user/resolvers/userResolvers.js
@@ -1,13 +1,7 @@
-const arrayUsers = [
-  { nome: "John Doe", ativo: true, email: "john@example.com" },
-  { nome: "Marcos", ativo: false, email: "marcos@example.com" },
-];
-
 const userResolvers = {
-    query : {
-        users: () => arrayUsers,
-        primeiroUser: () => arrayUsers[0],
-    },
+  query: {
+    users: ({ dataSources }) => dataSources.usersAPI.getUsers(),
+  },
 };
 
 module.exports = userResolvers;
