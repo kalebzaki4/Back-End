ce5e641d2b35c2d1ca4549239bd00ee1868022a1
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-04-02 19:36:41 -0300

    Update index.js

--- GraphQL/graphql-config-inicial/api/index.js
+++ GraphQL/graphql-config-inicial/api/index.js
@@ -5,7 +5,7 @@ const usersResolvers = require("../api/user/resolvers/userResolvers");
 const typeDefs = [usersSchema];
 const resolvers = [usersResolvers];
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, dataSources : () => ({ usersAPI: new UsersAPI() }) });
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Servidor rodando em ${url}`);
