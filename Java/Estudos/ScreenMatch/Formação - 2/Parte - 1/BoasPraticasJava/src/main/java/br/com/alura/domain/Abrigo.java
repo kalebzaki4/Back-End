1c7f80e5a34343b99191d00ea31359661b0b63b6
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-04-15 14:56:12 -0300

    Create Abrigo.java

---
+++ Java/Estudos/ScreenMatch/Formação - 2/Parte - 1/BoasPraticasJava/src/main/java/br/com/alura/domain/Abrigo.java
@@ -0,0 +1,15 @@
+package br.com.alura.domain;
+
+public class Abrigo {
+
+    public Abrigo(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    private String nome;
+    private String telefone;
+    private String email;
+
+}
