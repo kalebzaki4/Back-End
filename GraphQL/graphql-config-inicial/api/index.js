6b651c66eadd20328be2a56fc1a8246727cc3e81
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-04-30 11:24:41 -0300

    Iniciando os estudos de GraphQL
    Aprendendo o que é typedefs e resolvers.

--- GraphQL/graphql-config-inicial/api/index.js
+++ GraphQL/graphql-config-inicial/api/index.js
@@ -1,12 +1,3 @@
-const { ApolloServer } = require("apollo-server");
-const usersSchema = require("../api/user/schema/user.graphql");
-const usersResolvers = require("../api/user/resolvers/userResolvers");
+const { ApolloServer } = require('apollo-server');
 
-const typeDefs = [usersSchema];
-const resolvers = [usersResolvers];
-
-const server = new ApolloServer({ typeDefs, resolvers, dataSources : () => ({ usersAPI: new UsersAPI() }) });
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Servidor rodando em ${url}`);
-});
+const server = new ApolloServer({typeDefs, resolvers});
--- GraphQL/graphql-config-inicial/api/user/resolvers/userResolvers.js
+++
@@ -1,8 +0,0 @@
-const userResolvers = {
-  query: {
-    users: ({ dataSources }) => dataSources.usersAPI.getUsers(),
-    user: ({ dataSources }, { id }) => dataSources.usersAPI.getUser(id),
-  },
-};
-
-module.exports = userResolvers;
