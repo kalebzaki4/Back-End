c43fee177c60b8a5d8726f2e606ea878ca477e12
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-05-12 17:45:21 -0300

    Update Pet.java

--- Java/Estudos/Boas Práticas/Formação - 1/Parte - 1/boasPraticasJava/src/main/java/br/com/alura/domain/Pet.java
+++ Java/Estudos/Boas Práticas/Formação - 1/Parte - 1/boasPraticasJava/src/main/java/br/com/alura/domain/Pet.java
@@ -1,4 +1,20 @@
 package br.com.alura.domain;
 
 public class Pet {
+
+    public Pet(String tipo, String nome, String raca, int idade, String cor, Float peso) {
+        this.tipo = tipo;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.cor = cor;
+        this.peso = peso;
+    }
+
+    private String tipo;
+    private String nome;
+    private String raca;
+    private int idade;
+    private String cor;
+    private Float peso;
 }
