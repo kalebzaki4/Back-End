b8374cdd61ae421f7d5759074e18075f4a917ae2
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-05-14 14:34:42 -0300

    Create userResolvers.js

---
+++ GraphQL/Estudos/graphql-config-inicial/api/user/resolvers/userResolvers.js
@@ -0,0 +1,8 @@
+const userResolvers = {
+  Query: {
+    users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+    user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)
+  }
+}
+
+module.exports = userResolvers
