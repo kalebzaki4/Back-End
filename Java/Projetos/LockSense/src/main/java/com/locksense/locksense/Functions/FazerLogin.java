bd56921d43e3da64e5086ab0b52c69ca4d2a3ea4
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-06-03 18:14:39 -0300

    Update FazerLogin.java

--- Java/Projetos/LockSense/src/main/java/com/locksense/locksense/Functions/FazerLogin.java
+++ Java/Projetos/LockSense/src/main/java/com/locksense/locksense/Functions/FazerLogin.java
@@ -1,7 +1,28 @@
 package com.locksense.locksense.Functions;
 
+import java.util.Scanner;
+
 public class FazerLogin {
     public static void FazerLogin() {
-        System.out.println("Digite seu email: ");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu Gmail: ");
+        String gmail = scanner.nextLine();
+
+        String senha;
+        String confirmaSenha;
+
+        do {
+            System.out.print("Digite uma senha: ");
+            senha = scanner.nextLine();
+
+            System.out.print("Confirme a senha: ");
+            confirmaSenha = scanner.nextLine();
+
+            if (!senha.equals(confirmaSenha)) {
+                System.out.println("As senhas não conferem. Tente novamente.");
+            }
+        } while (!senha.equals(confirmaSenha));
+        System.out.println("Usuário cadastrado com sucesso!");
     }
 }
