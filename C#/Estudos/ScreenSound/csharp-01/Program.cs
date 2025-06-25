a74f07592560d6b4513558e1d59332491223cd74
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-06-25 11:08:16 -0300

    Update project to .NET 8 and improve welcome message
    Upgraded the TargetFramework in PrimeiroProjeto.csproj from net7.0 to net8.0. Refactored Program.cs to use a method for displaying the welcome message and added a prompt to press any key to continue. Added solution files and Visual Studio metadata for project organization.

--- C#/Estudos/ScreenSound/csharp-01/Program.cs
+++ C#/Estudos/ScreenSound/csharp-01/Program.cs
@@ -1,3 +1,8 @@
-﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-Console.WriteLine(mensagemDeBoasVindas);
+﻿void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine("Bem-vindo ao Screen Sound!");
+    Console.WriteLine("Pressione qualquer tecla para continuar...");
+}
+
+ExibirMensagemDeBoasVindas();
+Console.ReadKey();
