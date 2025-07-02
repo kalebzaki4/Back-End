dc06c305bffe07edbba37a726e977a691ca5ba74
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-02 12:48:28 -0300

    Add initial OrientacaoObjetos project and Program.cs
    Created a new C# project targeting .NET 8.0 for object-oriented studies. Added Program.cs with basic usage of a Musica class to demonstrate object instantiation and property assignment.

---
+++ C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Bohemian Rhapsody";
+musica1.artista = "Queen";
+
+Console.WriteLine($"Nome da musica {musica1.nome}");
+Console.WriteLine($"Artista da musica {musica1.artista}");
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Imagine";
+musica2.artista = "John Lennon";
+
+Console.WriteLine($"Nome da musica {musica2.nome}");
+Console.WriteLine($"Artista da musica {musica3.artista}");
