35a33b7878872f71f2745edeb0d9d2bb789db1ec
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-03 15:24:16 -0300

    Refatora classe Musica para usar propriedades
    Substitui campos públicos por propriedades automáticas na classe Musica e atualiza o uso dessas propriedades no Program.cs. Adiciona também a propriedade NomeCompleto à classe Musica.

--- C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/Program.cs
+++ C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/Program.cs
@@ -1,14 +1,15 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Bohemian Rhapsody";
-musica1.artista = "Queen";
-musica1.duracao = 354;
-musica1.disponivel = true;
+musica1.Nome = "Bohemian Rhapsody";
+musica1.Artista = "Queen";
+musica1.Duracao = 354;
+musica1.Disponivel = true;
+System.Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
-musica2.nome = "Imagine";
-musica2.artista = "John Lennon";
-musica2.duracao = 183;
-musica2.disponivel = false;
+musica2.Nome = "Imagine";
+musica2.Artista = "John Lennon";
+musica2.Duracao = 183;
+musica2.Disponivel = false;
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
