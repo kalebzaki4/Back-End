5524511b985767df3ce80b84e48392ed0ab8746e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-03 13:58:12 -0300

    Add ExibirFichaTecnica method to Musica class
    Introduces the ExibirFichaTecnica method to the Musica class, allowing detailed display of music information including name, artist, duration, and availability status.

--- C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/musica.cs
+++ C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/musica.cs
@@ -1,8 +1,22 @@
 class Musica
 {
-    // Atributos
     public string nome;
     public string artista;
     public int duracao;
     public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao} segundos");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano plus+    ");
+        }
+    }
 }
