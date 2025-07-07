3b084fadd73d7ca2df68212e79e8b11d742bb2b3
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-07 12:26:50 -0300

    Add solution file and rename property in Musica class
    Added the Visual Studio solution file for the project. Renamed the 'NomeCompleto' property to 'DescricaoResumida' in the Musica class to better reflect its purpose.

--- C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/musica.cs
+++ C#/Estudos/ScreenSound/Parte - 2/OrientacaoObjetos/musica.cs
@@ -4,7 +4,7 @@ class Musica
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-    public string NomeCompleto { get; set; }
+    public string DescricaoResumida  { get; set; }
 
     public void ExibirFichaTecnica()
     {
