094c7ea843b47252c169df834613016dae743050
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-07 15:55:35 -0300

    Update Program.cs

--- C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Program.cs
+++ C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Program.cs
@@ -1,15 +1,12 @@
-﻿Musica musica1 = new Musica();
-musica1.Nome = "Roxane";
-musica1.Artista = "The Police";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
-Console.WriteLine(musica1.DescricaoResumida);
+﻿Album albumDoQueen = new Album();
+albumDoQueen.Nome = "A Night at the Opera";
+
+Musica musica1 = new Musica();
+musica1.Nome = "Love of My Life";
 
 Musica musica2 = new Musica();
-musica2.Nome = "Vertigo";
-musica2.Artista = "U2";
-musica2.Duracao = 367;
-musica2.Disponivel = false;
+musica2.Nome = "Bohemian Rhapsody";
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
