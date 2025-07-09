94e087029298f02a1920a3cadd02b880d0056d84
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-09 12:32:57 -0300

    Update Musica.cs

--- C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Musica.cs
+++ C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Musica.cs
@@ -4,7 +4,11 @@
     public string Artista { get; set; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
-    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
+    public string DescricaoResumida => 
+        $"A música {Nome} pertence à banda {Artista}";
+
+
+
 
     public void ExibirFichaTecnica()
     {
@@ -14,8 +18,7 @@
         if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
-        }
-        else
+        } else
         {
             Console.WriteLine("Adquira o plano Plus+");
         }
