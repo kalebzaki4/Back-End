e9ffbbe0c8d841b2724d0d505902e3398cf27f70
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-10 10:56:00 -0300

    Update Program.cs

--- C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Program.cs
+++ C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Program.cs
@@ -1,15 +1,24 @@
-﻿Album albumDoQueen = new Album();
-albumDoQueen.Nome = "A night at the opera";
+﻿Banda queen = new Banda("Queen");
 
-Musica musica1 = new Musica();
-musica1.Nome = "Love of my life";
-musica1.Duracao = 231;
+Album albumDoQueen = new Album("A night at the opera");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Bohemian Rhapsody";
-musica2.Duracao = 354;
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
 
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
 albumDoQueen.ExibirMusicasDoAlbum();
+queen.ExibirDiscografia();
