fdfa6f5a4ff4b663139708df4f9e67115ce72eaf
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-10 10:55:57 -0300

    Create Banda.cs

---
+++ C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Banda.cs
@@ -0,0 +1,25 @@
+﻿class Banda
+{
+    private List<Album> albums = new List<Album>();
+
+    public Banda(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+
+    public void AdicionarAlbum(Album album) 
+    { 
+        albums.Add(album);
+    }
+
+    public void ExibirDiscografia()
+    {
+        Console.WriteLine($"Discografia da banda {Nome}");
+        foreach (Album album in albums)
+        {
+            Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+        }
+    }
+}
