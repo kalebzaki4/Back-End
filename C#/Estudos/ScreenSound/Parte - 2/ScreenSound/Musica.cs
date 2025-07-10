ca6d1fd06166fe4950e96a81e7daf690a19d8e74
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-10 10:55:59 -0300

    Update Musica.cs

--- C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Musica.cs
+++ C#/Estudos/ScreenSound/Parte - 2/ScreenSound/Musica.cs
@@ -1,7 +1,13 @@
 ﻿class Musica
 {
-    public string Nome { get; set; }
-    public string Artista { get; set; }
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista { get; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
     public string DescricaoResumida => 
@@ -13,7 +19,7 @@
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
-        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
         Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
