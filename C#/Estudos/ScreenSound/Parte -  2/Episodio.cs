92b0e129bb0f5863745163d0fa720ae7785797b5
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-11 10:10:46 -0300

    Create Episodio.cs

---
+++ C#/Estudos/ScreenSound/Parte -  2/Episodio.cs
@@ -0,0 +1,20 @@
+﻿class Episodio
+{
+    private List<string> convidados = new();
+    public Episodio(int ordem, string titulo, int duracao)
+    {
+        Ordem = ordem;
+        Titulo = titulo;
+        Duracao = duracao;
+    }
+
+    public int Ordem { get; }
+    public string Titulo { get; }
+    public int Duracao { get; }
+    public string Resumo => $"{Ordem}. {Titulo} ({Duracao} min) - {string.Join(", ", convidados)}";
+
+    public void AdicionarConvidado(string convidado)
+    {
+        convidados.Add(convidado);
+    }
+}
