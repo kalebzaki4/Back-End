7ce06c8771f19eafa8175058d765da19e9bdf500
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-11 10:10:49 -0300

    Create Podcast.cs

---
+++ C#/Estudos/ScreenSound/Parte -  2/Podcast.cs
@@ -0,0 +1,29 @@
+﻿class Podcast
+{
+    private List<Episodio> episodios = new();
+
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    public string Nome { get; }
+    public string Host { get; }
+    public int TotalEpisodios => episodios.Count;
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast >|{Nome}|< apresentado por [{Host}]\n");
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+        Console.WriteLine($"\n\nTotal de episódios: {TotalEpisodios}.");
+    }
+}
