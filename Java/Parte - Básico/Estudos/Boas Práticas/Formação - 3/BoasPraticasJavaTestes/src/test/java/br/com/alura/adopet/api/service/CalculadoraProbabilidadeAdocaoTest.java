794c8d1dc716741358227844c27d802c6574a37d
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-07-22 15:44:53 -0300

    Create CalculadoraProbabilidadeAdocaoTest.java

---
+++ Java/Parte - Básico/Estudos/Boas Práticas/Formação - 3/BoasPraticasJavaTestes/src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,13 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    @Test
+    void cenario01() {
+
+    }
+}
