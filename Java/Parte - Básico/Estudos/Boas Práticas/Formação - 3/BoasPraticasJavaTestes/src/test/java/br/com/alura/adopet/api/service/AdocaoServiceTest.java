f99de62c23ef9f9340fd0b6dd5ed4b3098f0c4aa
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-08-04 16:04:16 -0300

    Create AdocaoServiceTest.java

---
+++ Java/Parte - Básico/Estudos/Boas Práticas/Formação - 3/BoasPraticasJavaTestes/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,15 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdocaoServiceTest {
+
+
+    @Test
+    void deveriaSalvarAdocaoAoSolicitar() {
+        
+    }
+
+}
