0e8c3702a0f81295c27730748b51a9cf5ae6d85b
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-08-26 17:36:29 -0300

    Refactor login test setup into LoginPage class
    Moved WebDriver setup and teardown logic from LoginTest to a new LoginPage class to improve code organization and reuse. LoginTest now uses LoginPage for browser management.

--- Java/Básico/Estudos/Boas Práticas e Testes/Curso - 3/TestesDeAceitação/src/test/java/br/com/alura/leilao/LoginTest.java
+++ Java/Básico/Estudos/Boas Práticas e Testes/Curso - 3/TestesDeAceitação/src/test/java/br/com/alura/leilao/LoginTest.java
@@ -13,24 +13,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class LoginTest {
 
-    public static final String LOGIN = "http://localhost:8080/login";
-    private WebDriver browser;
-
-    @BeforeAll
-    public static void BeforeAll() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\kalebzaki\\Documents\\GitHub\\Back-End\\Java\\Básico\\Estudos\\Boas Práticas e Testes\\Curso - 3\\TestesDeAceitação\\drivers\\chromedriver.exe");
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        this.browser = new ChromeDriver(new ChromeOptions().setBinary("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"));
-        browser.navigate().to("http://localhost:8080/login");
-    }
-
-    @AfterEach
-    public void AfterEach() {
-        this.browser.quit();
-    }
+    private LoginPage paginaDeLogin;
 
     @Test
     public void deveFazerLoginComSucesso() {
