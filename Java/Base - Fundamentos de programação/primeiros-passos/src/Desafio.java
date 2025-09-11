8d50f0dcbaaed399e5bb34428a656d8905939f18
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2025-09-11 14:22:15 -0300

    Create Desafio.java

---
+++ Java/Base - Fundamentos de programação/primeiros-passos/src/Desafio.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nome = "Clark Kent";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
+
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n***********************");
+
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor 
+                4 - Sair
+                
+                """;
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual o valor que deseja transferir?");
+                double valor = leitura.nextDouble();
+                if (valor > saldo) {
+                    System.out.println("Não há saldo para realizar a transferência.");
+                } else {
+                    saldo -= valor;
+                    System.out.println("Novo saldo: " + saldo);
+                }
+            } else if (opcao == 3) {
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
+            }
+        }
+    }
+}
